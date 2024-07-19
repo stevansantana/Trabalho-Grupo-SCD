8c18ebce06e23ad6e91d6ce3f92baebd360e42ce
Stevan <stevansantanas@gmail.com>
2024-07-18 23:49:55 -0300

    Iniciando a implementação do Processo

---
+++ processo.py
@@ -0,0 +1,28 @@
+import socket
+
+class Processo:
+    def __init__(self, host, porta, id_processo, repeticoes, intervalo):
+        self.host = host
+        self.porta = porta
+        self.id_processo = id_processo
+        self.repeticoes = repeticoes
+        self.intervalo = intervalo
+        self.cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        
+    def iniciar(self):
+        for _ in range(self.repeticoes):
+            self.solicitar_acesso()
+            self.regiao_critica()
+            self.liberar_acesso()
+            
+    def solicitar_acesso(self):
+        pass
+    
+    def regiao_critica(self):
+        pass
+    
+    def liberar_acesso(self):
+        pass
+    
+        
+    
